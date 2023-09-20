1f4abe91509732e820bfa68d30929b89e427d9e7
koushith <koushith97@gmail.com>
2023-09-21 02:22:20 +0530

    adds landing page

--- frontend/src/screens/landing-page/components/challange/challange.section.tsx
+++ frontend/src/screens/landing-page/components/challange/challange.section.tsx
@@ -1,9 +1,62 @@
+import styled from "styled-components";
 import { Heading } from "../primitives/heading/heading.component";
 
 export const ChallangeSection = () => {
   return (
-    <>
-      <Heading text="The chalange" />
-    </>
+    <ChallangeContainer>
+      <div className="challenge-container">
+        <div className="left">
+          <div className="thumbnail"></div>
+        </div>
+
+        <div className="right">
+          <Heading text="The chalenge" />
+
+          <p>Send in your ideas and if it's actually good,</p>
+          <p>we will produce it this time.</p>
+          <p>Are you talanted enough to make us eat our words?</p>
+          <button>Submit your pitch</button>
+        </div>
+      </div>
+    </ChallangeContainer>
   );
 };
+
+export const ChallangeContainer = styled.section`
+  margin-bottom: 4rem;
+  .challenge-container {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    justify-content: center;
+
+    .left {
+      .thumbnail {
+        background-color: black;
+        height: 300px;
+        width: 200px;
+        border-radius: 10px;
+      }
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+
+      p {
+        text-align: center;
+      }
+
+      button {
+        background-color: #d8d8d8;
+        padding: 0.6rem 2rem;
+        border-radius: 50px;
+        margin-top: 2rem;
+        -webkit-box-shadow: -15px 14px 11px -1px rgba(0, 0, 0, 0.75);
+        -moz-box-shadow: -15px 14px 11px -1px rgba(0, 0, 0, 0.75);
+        box-shadow: -15px 14px 11px -1px rgba(0, 0, 0, 0.75);
+      }
+    }
+  }
+`;
--- frontend/src/screens/landing-page/components/hero/hero.section.tsx
+++ frontend/src/screens/landing-page/components/hero/hero.section.tsx
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+import Hero from "../../assets/Backgound/hero.png";
+
 export const HeroSection = () => {
-  return <>Section</>;
+  return (
+    <HeroContainer>
+      <img src={Hero} alt="hero" />
+    </HeroContainer>
+  );
 };
+
+export const HeroContainer = styled.section`
+  position: relative;
+`;
